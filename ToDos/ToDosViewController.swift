67249b205b5949b028e34997a367a55e16e93994
aaronorr8 <aaronorr8@gmail.com>
2019-06-01 07:29:09 -0600

    Started tableview

---
+++ ToDos/ToDosViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  ToDos
+//
+//  Created by Aaron Orr on 6/1/19.
+//  Copyright © 2019 Aaron. All rights reserved.
+//
+
+import UIKit
+
+class ToDosViewController: UITableViewController {
+    
+    let itemArray = ["Go karts", "Mountain bike", "Surfing"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+    }
+    
+    //MARK - Tableview Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(itemArray[indexPath.row])
+        
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        
+    }
+
+
+}
+
