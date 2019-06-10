14c9676fac6c5c73df017012401796b4b1092b12
aaronorr8 <aaronorr8@gmail.com>
2019-06-10 08:01:55 -0600

    Added Category screen UI

--- ToDos/Controllers/ToDosViewController.swift
+++ ToDos/Controllers/ToDosViewController.swift
@@ -11,6 +11,8 @@ import CoreData
 
 class ToDosViewController: UITableViewController {
     
+
+    
     var itemArray = [Item]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
@@ -107,15 +109,49 @@ class ToDosViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-    func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest()) {
+
         do {
             itemArray =  try context.fetch(request)
         } catch {
             print("Error fetching data from context \(error)")
         }
+        
+        tableView.reloadData()
+    }
+    
+   
+    
+
+}
+
+//MARK: Search bar methods
+extension ToDosViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        
+        request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        
+        loadItems(with: request)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if searchBar.text?.count == 0 {
+            loadItems()
+            
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
+            }
+            
+            
+        } 
     }
     
 
+    
 }
 
